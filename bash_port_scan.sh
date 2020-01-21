90277167444131aa9cce4d418bc8cfcb7c4e9f03
trenerok <pcdoctorby@gmail.com>
2020-01-21 22:51:28 +0300

    Update bash_port_scan.sh

--- bash_port_scan.sh
+++ bash_port_scan.sh
@@ -12,7 +12,7 @@ for IP in $(seq 1 254); do
 			echo "START scanning - $SUBNET.$IP"
             for PORT in $(seq $2 $3); do
             #echo "scanning - $SUBNET.$IP $PORT"
-            ./ncat -nv -w 1 -i 1 $SUBNET.$IP $PORT 2>&1 | grep open
+            ./ncat -nv -w 1 -i 1 $SUBNET.$IP $PORT 2>&1 | grep Connected
             done
             echo "FINISH scanning - $SUBNET.$IP"
 done
