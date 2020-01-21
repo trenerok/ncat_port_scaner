363b86995b753e41345728309ca0691646fbf1ce
pslizh <p_slizh@L1061.local>
2020-01-21 22:09:33 +0300

    Initial commit

---
+++ bash_port_scan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Echo “enter your subnet”
+echo "You provided $# arguments - scan ip range: $1 | port range: FROM $2 TO $3"
+SUBNET=$1
+PORT_FROM=$2
+PORT_TO=$3
+
+
+for IP in $(seq 15 17); do
+	        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+			echo "START scanning - $SUBNET.$IP"
+            for PORT in $(seq $2 $3); do
+            #echo "scanning - $SUBNET.$IP $PORT"
+            ./ncat -z -nv -w 1 $SUBNET.$IP $PORT 2>&1 | grep open
+            done
+            echo "FINISH scanning - $SUBNET.$IP"
+done
+echo "done"
