a1e2c013e5a0ad5c129ea5261cc686c93275ed16
trenerok <pcdoctorby@gmail.com>
2020-01-22 12:47:26 +0300

    Update bash_port_scan.sh

--- bash_port_scan.sh
+++ bash_port_scan.sh
@@ -5,15 +5,16 @@ echo "You provided $# arguments - scan ip range: $1 | port range: FROM $2 TO $3"
 SUBNET=$1
 PORT_FROM=$2
 PORT_TO=$3
+FILENAME=result_ncat_scan_$1.log
 
 
 for IP in $(seq 1 254); do
-	        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-			echo "START scanning - $SUBNET.$IP"
+	        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> "$FILENAME"
+			echo "START scanning - $SUBNET.$IP" >> "$FILENAME"
             for PORT in $(seq $2 $3); do
             #echo "scanning - $SUBNET.$IP $PORT"
             ./ncat -nv -w 1 -i 1 $SUBNET.$IP $PORT 2>&1 | grep Connected
             done
-            echo "FINISH scanning - $SUBNET.$IP"
+            echo "FINISH scanning - $SUBNET.$IP" >> "$FILENAME"
 done
-echo "done"
+echo "done" >> "$FILENAME"
